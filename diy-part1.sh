fb9493692bc0683570bb21503bc3f6b2690350d8
beiyuqu <326936595@qq.com>
2020-12-05 21:05:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,4 @@
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
-git clone https://github.com/sirpdboy/luci-theme-opentopd package/
-scripts/feeds install luci-theme-opentopd
-
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/
