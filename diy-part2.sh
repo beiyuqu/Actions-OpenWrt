d949c1087ae73842e4b246b458a1eaf2dd294ea9
beiyuqu <326936595@qq.com>
2020-12-05 21:04:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # 删除默认主题
-# sed -i 's/+luci-theme-bootstrap//g' feeds/luci/collections/luci/Makefile
+sed -i 's/+luci-theme-bootstrap//g' feeds/luci/collections/luci/Makefile
 
 # 设置默认密码
 sed -i 's/^root.*/root:$1$tPYTHhT8$XRiZ79P8LHjTJvevrFt.F1:18473:0:99999:7:::/g' package/base-files/files/etc/shadow
